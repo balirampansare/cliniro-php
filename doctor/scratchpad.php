696ac2c778267306a843920a3050105c98c4c1b5
Baliram Pansare <baliram.psmbn@gmail.com>
2023-02-17 22:21:13 +0530

    trying out drawing board page

--- doctor/scratchpad.php
+++ doctor/scratchpad.php
@@ -318,82 +318,77 @@ if(strlen($_SESSION['id']==0)) {
   <main id="main" class="main">
 
 <div class="pagetitle">
-  <h1>Clifea</h1>
+  <h1>Drawing Board</h1>
   <nav>
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-      <li class="breadcrumb-item active">Clifea</li>
+      <li class="breadcrumb-item active">Drawing Board</li>
     </ol>
   </nav>
 </div><!-- End Page Title -->
 
-<section class="section dashboard">
+<section class="section ">
+<div class="container2">
+      <section class="tools-board">
+        <div class="row">
+          <label class="title">Shapes</label>
+          <ul class="options">
+            <li class="option tool" id="rectangle">
+              <img src="icons/rectangle.svg" alt="">
+              <span>Rectangle</span>
+            </li>
+            <li class="option tool" id="circle">
+              <img src="icons/circle.svg" alt="">
+              <span>Circle</span>
+            </li>
+            <li class="option tool" id="triangle">
+              <img src="icons/triangle.svg" alt="">
+              <span>Triangle</span>
+            </li>
+            <li class="option">
+              <input type="checkbox" id="fill-color">
+              <label for="fill-color">Fill color</label>
+            </li>
+          </ul>
+        </div>
+        <div class="row">
+          <label class="title">Options</label>
+          <ul class="options">
+            <li class="option active tool" id="brush">
+              <img src="icons/brush.svg" alt="">
+              <span>Brush</span>
+            </li>
+            <li class="option tool" id="eraser">
+              <img src="icons/eraser.svg" alt="">
+              <span>Eraser</span>
+            </li>
+            <li class="option">
+              <input type="range" id="size-slider" min="1" max="30" value="5">
+            </li>
+          </ul>
+        </div>
+        <div class="row colors">
+          <label class="title">Colors</label>
+          <ul class="options">
+            <li class="option"></li>
+            <li class="option selected"></li>
+            <li class="option"></li>
+            <li class="option"></li>
+            <li class="option">
+              <input type="color" id="color-picker" value="#4A98F7">
+            </li>
+          </ul>
+        </div>
+        <div class="row buttons">
+          <button class="clear-canvas">Clear Canvas</button>
+          <button class="save-img">Save As Image</button>
+        </div>
+      </section>
+      <section class="drawing-board">
+        <canvas></canvas>
+      </section>
+    </div>
 
-<!-- -------------------------------------------START OF DRAWING PAD ROW------------- -->
-
-<div class="container2 mt-3">
-
-<section class="tools-board">
-<div class="drawrow">
-  <label class="title">Shapes</label>
-  <ul class="options">
-    <li class="option tool" id="rectangle">
-      <img src="assets/img/rectangle.svg" alt="">
-      <span>Rectangle</span>
-    </li>
-    <li class="option tool" id="circle">
-      <img src="assets/img/circle.svg" alt="">
-      <span>Circle</span>
-    </li>
-    <li class="option tool" id="triangle">
-      <img src="assets/img/triangle.svg" alt="">
-      <span>Triangle</span>
-    </li>
-    <li class="option">
-      <input type="checkbox" id="fill-color" name="fill-color">
-      <label for="fill-color">Filled shape</label>
-    </li>
-  </ul>
-</div>
-<div class="drawrow">
-  <label class="title">Options</label>
-  <ul class="options">
-    <li class="option active tool" id="brush">
-      <img src="assets/img/brush.svg" alt="">
-      <span>Brush</span>
-    </li>
-    <li class="option tool" id="eraser">
-      <img src="assets/img/eraser.svg" alt="">
-      <span>Eraser</span>
-    </li>
-    <li class="option">
-      <input type="range" id="size-slider" min="1" max="30" value="5">
-    </li>
-  </ul>
-</div>
-<div class="drawrow colors">
-  <label class="title">Colors</label>
-  <ul class="options">
-    <li class="option"></li>
-    <li class="option selected"></li>
-    <li class="option"></li>
-    <li class="option"></li>
-    <li class="option">
-      <input type="color" id="color-picker" value="#4A98F7">
-    </li>
-  </ul>
-</div>
-<div class="d-flex my-2">
-  <button class="btn btn-outline-success fw-bold btn-sm" id="clear-canvas">Clear Canvas</button>
-  <button class="btn btn-outline-success fw-bold btn-sm ms-1" id="save-img">Save As Image</button>
-</div>
-</section>
-
-<section class="drawing-board rounded">
-<canvas></canvas>
-</section>
-
-</div>
 
       
 </section>
@@ -433,6 +428,7 @@ if(strlen($_SESSION['id']==0)) {
   <!-- Template Main JS File -->
 
   <script src="assets/js/main.js"></script>
+  <script src="assets/js/draw.js"></script>
 
 </body>
 
