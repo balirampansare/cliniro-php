f343bb5f02fc0e7b63b18c4a5e480361d3468781
Baliram Pansare <baliram.psmbn@gmail.com>
2023-03-14 20:56:55 +0530

    commented out the unrequired code of landing page

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ error_reporting(0);
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>You care for patients we care for you</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h2></h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="doctor/doctorlogin.php" class="btn-get-started scrollto">Login</a>
             <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
@@ -131,9 +131,9 @@ error_reporting(0);
 
   </section><!-- End Hero -->
 
-  <main id="main">
+  <!--main id="main">
 
-    <!-- ======= Clients Section ======= -->
+   
     <section id="clients" class="clients section-bg">
       <div class="container">
 
@@ -166,9 +166,9 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Cliens Section -->
+    </section><End Cliens Section -->
 
-    <!-- ======= About Us Section ======= -->
+    <!-- ======= About Us Section ======= >
     <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
@@ -199,9 +199,9 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End About Us Section -->
+    </section>
 
-    <!-- ======= Why Us Section ======= -->
+    
     <section id="why-us" class="why-us section-bg">
       <div class="container-fluid" data-aos="fade-up">
 
@@ -254,9 +254,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Why Us Section -->
+    </section>
 
-    <!-- ======= Skills Section ======= -->
     <section id="skills" class="skills">
       <div class="container" data-aos="fade-up">
 
@@ -307,9 +306,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Skills Section -->
+    </section>
 
-    <!-- ======= Services Section ======= -->
     <section id="services" class="services section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -354,9 +352,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Services Section -->
+    </section>
 
-    <!-- ======= Cta Section ======= -->
     <section id="cta" class="cta">
       <div class="container" data-aos="zoom-in">
 
@@ -371,9 +368,9 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Cta Section -->
+    </section>
 
-    <!-- ======= Portfolio Section ======= -->
+
     <section id="portfolio" class="portfolio">
       <div class="container" data-aos="fade-up">
 
@@ -484,9 +481,9 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Portfolio Section -->
+    </section>
 
-    <!-- ======= Team Section ======= -->
+
     <section id="team" class="team section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -568,9 +565,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Team Section -->
+    </section>
 
-    <!-- ======= Pricing Section ======= -->
     <section id="pricing" class="pricing">
       <div class="container" data-aos="fade-up">
 
@@ -629,9 +625,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Pricing Section -->
+    </section>
 
-    <!-- ======= Frequently Asked Questions Section ======= -->
     <section id="faq" class="faq section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -691,9 +686,8 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Frequently Asked Questions Section -->
+    </section>
 
-    <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
 
@@ -708,7 +702,6 @@ error_reporting(0);
             <div class="info">
               <form method="post" name="submit">
                 <div class="row jumbotron rounded py-2">
-                    <!--div class="col-sm-12 form-group"-->
                     <div class="col-sm-5 form-group text-center  m-auto">
                       <label for="typeofreg">Select Type</label>
                       <select name="typeofreg" id="typeofreg" class="form-control browser-default custom-select">
@@ -757,7 +750,7 @@ error_reporting(0);
                         <div class="col-sm-12 form-group mt-1">
                             <button type="submit" name="submit"  class="btn btn-outline-success float-end">Submit</button>
                         </div>
-                    <!--/div-->
+
                 </div>
     
             </form>
@@ -799,11 +792,10 @@ error_reporting(0);
         </div>
 
       </div>
-    </section><!-- End Contact Section -->
+    </section>
 
-  </main><!-- End #main -->
+  </main>
 
-  <!-- ======= Footer ======= -->
   <footer id="footer">
 
     <div class="footer-newsletter">
@@ -877,15 +869,9 @@ error_reporting(0);
       <div class="copyright">
         &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+      
     </div>
-  </footer><!-- End Footer -->
+  </footer-->
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
